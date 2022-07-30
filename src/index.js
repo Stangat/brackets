e2ddfc798121a655b26300bdd039ebc2bf2472ad
Stas Gatilov <stasgat12@gmail.com>
2022-07-30 12:23:34 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const OPEN_BRACKETS = ["(", "{", "[", "|", "1", "3", "5", "7", "8"];
+
+  const BRACKETS_PAIR = {
+    [")"]: "(",
+    ["}"]: "{",
+    ["]"]: "[",
+    ["|"]: "|",
+    ["2"]: "1",
+    ["4"]: "3",
+    ["6"]: "5",
+    ["7"]: "7",
+    ["8"]: "8",
+  };
+
+  const stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let currSymb = str[i];
+    if (
+      OPEN_BRACKETS.includes(currSymb) &&
+      (stack[stack.length - 1] !== "|" || currSymb !== "|") &&
+      (stack[stack.length - 1] !== "7" || currSymb !== "7") &&
+      (stack[stack.length - 1] !== "8" || currSymb !== "8")
+    ) {
+      stack.push(currSymb);
+    } else {
+      let stackTopElement = stack[stack.length - 1];
+      if (BRACKETS_PAIR[currSymb] === stackTopElement) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
